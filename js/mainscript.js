fbff96f9d2e1f15a2c4abd6542ba04518d85d951
aymanazm <aymanmohamed9600@gmail.com>
2017-12-20 19:41:53 +0200

    -

--- js/mainscript.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {                            // On Window Load
-    var inputForm   = document.querySelector('form');   // Select Form
-    inputForm.onsubmit = function(e) {                  // Form On Submit
-        if (!inputForm.img_to_compress.value) {         // if File Upload Field is Empty   
-            e.preventDefault();                         // Pervent Form Submit (Return False)
-            alert('Please Select an Image!');           // Alert An Error Message
-        }
-    }
-}
